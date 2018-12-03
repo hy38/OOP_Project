13184b93471a6402ecdb0c93a0858309da63f6d7
ohk1119 <5ghdrl@daum.net>
2018-12-03 04:24:53 -0800

    leaveRequest

---
+++ LeaveRequest.h
@@ -0,0 +1,17 @@
+#ifndef LEAVEREQUEST_H
+#define LEAVEREQUEST_H
+#include "Member.h"
+#include <string>
+
+class LeaveRequest : public Member {
+
+    private:
+        string RequestList[];   // 신청을 받는다면 main에서 저장할지? 아니면 member들의 질문 리스트로 가질 지.
+        
+    public:
+        LeaveRequest();
+        void addRequest(string _questionList[], int _questionNumber);
+        bool ifRequest();
+};
+
+#endif
